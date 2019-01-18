dbc466ace134b59cb3af1ae2bd1659813f09dc80
Quan Wang <quanw@google.com>
2019-01-18 13:57:06 -0500

    Add input check to refinement

--- spectralcluster/refinement.py
+++ spectralcluster/refinement.py
@@ -3,6 +3,24 @@ import numpy as np
 
 
 class AffinityRefinementOperation(metaclass=abc.ABCMeta):
+    def check_input(self, X):
+        """Check the input to the refine() method.
+
+        Args:
+            X: the input to the refine() method
+
+        Raises:
+            TypeError: if X has wrong type
+            ValueError: if X has wrong shape, etc.
+        """
+        if not isinstance(X, np.ndarray):
+            raise TypeError("X must be a numpy array")
+        shape = X.shape
+        if len(shape) != 2:
+            raise ValueError("X must be 2-dimensional")
+        if shape[0] != shape[1]:
+            raise ValueError("X must be a square matrix")
+
     @abc.abstractmethod
     def refine(self, X):
         """Perform the refinement operation.
@@ -19,4 +37,5 @@ class AffinityRefinementOperation(metaclass=abc.ABCMeta):
 class Diffuse(AffinityRefinementOperation):
     """The diffusion operation."""
     def refine(self, X):
+        self.check_input(X)
         return np.matmul(X, np.transpose(X))
