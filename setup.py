85c9f36cbeb98354ff8e8f38be21662ca06dc07b
Quan Wang <quanw@google.com>
2019-01-18 17:49:21 -0500

    Fix flake8 errors for setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 VERSION = "0.0.1"
 
 with open("README.md", "r") as file_object:
-  LONG_DESCRIPTION = file_object.read()
+    LONG_DESCRIPTION = file_object.read()
 
 setuptools.setup(
     name="spectralcluster",
