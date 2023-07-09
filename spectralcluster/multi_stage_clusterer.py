69c8c03f8b855ab1aac9c4a3b9610f112c593e9c
Quan Wang <quanw@google.com>
2023-07-08 21:06:00 -0400

    [WIP] Start working on MultiStageClusterer

---
+++ spectralcluster/multi_stage_clusterer.py
@@ -0,0 +1,98 @@
+"""Implementation of multi-stage clustering.
+
+Multi-stage clustering class is introduced in this paper:
+
+* Quan Wang, Yiling Huang, Han Lu, Guanlong Zhao, Ignacio Lopez Moreno,
+  "Highly efficient real-time streaming and fully on-device speaker
+  diarization with multi-stage clustering." arXiv preprint
+  arXiv:2210.13690 (2022).
+"""
+
+import numpy as np
+from sklearn.cluster import AgglomerativeClustering
+from spectralcluster import fallback_clusterer
+from spectralcluster import spectral_clusterer
+from spectralcluster import utils
+
+
+class MultiStageClusterer:
+  """Multi-stage clustering class."""
+
+  def __init__(
+      self,
+      main_clusterer: spectral_clusterer.SpectralClusterer,
+      fallback_threshold: float = 0.5,
+      L: int = 50,
+      U1: int = 100,
+      U2: int = 600,
+  ):
+    # Main clusterer.
+    self.main = main_clusterer
+
+    if self.main.max_spectral_size:
+      raise ValueError(
+        "Do not set max_spectral_size for SpectralClusterer when"
+        "using MultiStageClusterer.")
+
+    # Lower bound of main clusterer.
+    self.main.fallback_options.spectral_min_embeddings = L
+
+    # Upper bound of main clusterer.
+    self.U1 = U1
+
+    # Upper bound of pre-clusterer.
+    self.U2 = U2
+
+    # Threshold for fallback AHC clusterer.
+    self.main.fallback_options.agglomerative_threshold = fallback_threshold
+
+    # Other configs for fallback.
+    self.main.fallback_options.single_cluster_condition = (
+      fallback_clusterer.SingleClusterCondition.FallbackClusterer)
+    self.main.fallback_options.fallback_clusterer_type = (
+      fallback_clusterer.FallbackClustererType.Agglomerative)
+
+    # Pre-clusterer.
+    self.pre = AgglomerativeClustering(
+        n_clusters=U1,
+        metric="cosine",
+        linkage="complete")
+
+    # All cached centroids.
+    self.cache = None
+
+    # Number of clustered embeddings.
+    self.num_embeddings = 0
+
+  def streaming_predict(
+      self,
+      embedding: np.ndarray
+  ) -> np.ndarray:
+    """A streaming prediction function.
+
+    Note that this is not a simple online prediction class It not only
+    predicts the label of the next input, but also makes corrections to
+    previously predicted labels.
+    """
+    self.num_embeddings += 1
+
+    # First input.
+    if self.num_embeddings == 1:
+      self.cache = embedding
+      return np.array([0])
+
+    # Using fallback or main clusterer only.
+    if self.num_embeddings <= self.U1:
+      self.cache = np.vstack([self.cache, embedding])
+      return self.main.predict(self.cache)
+
+    # Run preclusterer.
+    if self.num_embeddings <= self.U2:
+      self.cache = np.vstack([self.cache, embedding])
+      pre_labels = self.pre.fit_predict(self.cache)
+      pre_centroids = utils.get_cluster_centroids(self.cache, pre_labels)
+      main_labels = self.main.predict(pre_centroids)
+      return utils.chain_labels(pre_labels, main_labels)
+
+    # TODO: Dynamic compression.
+    raise NotImplementedError("Dynamic compression not implemented yet.")
---
+++ tests/multi_stage_clusterer_test.py
@@ -0,0 +1,74 @@
+import unittest
+
+import numpy as np
+from spectralcluster import configs
+from spectralcluster import multi_stage_clusterer
+from spectralcluster import refinement
+from spectralcluster import spectral_clusterer
+from spectralcluster import utils
+
+
+class TestMultiStageClusterer(unittest.TestCase):
+  """Tests for the MultiStageClusterer class."""
+
+  def setUp(self):
+    super().setUp()
+    refinement_options = refinement.RefinementOptions(
+        gaussian_blur_sigma=0,
+        p_percentile=0.95,
+        refinement_sequence=configs.ICASSP2018_REFINEMENT_SEQUENCE)
+    main_clusterer = spectral_clusterer.SpectralClusterer(
+        refinement_options=refinement_options)
+    self.multi_stage = multi_stage_clusterer.MultiStageClusterer(
+      main_clusterer=main_clusterer,
+      fallback_threshold=0.5,
+      L=3,
+      U1=5,
+      U2=7
+    )
+
+  def test_single_input(self):
+    embedding = np.array([[1, 2]])
+    labels = self.multi_stage.streaming_predict(embedding)
+    expected = np.array([0])
+    np.testing.assert_equal(expected, labels)
+
+  def test_fallback(self):
+    embeddings = [[1, 2], [3, -1]]
+    for embedding in embeddings:
+      labels = self.multi_stage.streaming_predict(np.array(embedding))
+    labels = utils.enforce_ordered_labels(labels)
+    expected = np.array([0, 1])
+    np.testing.assert_equal(expected, labels)
+
+  def test_main(self):
+    embeddings = [
+      [1, 2],
+      [3, -1],
+      [1, 1],
+      [-2, -1],
+      ]
+    for embedding in embeddings:
+      labels = self.multi_stage.streaming_predict(np.array(embedding))
+    labels = utils.enforce_ordered_labels(labels)
+    expected = np.array([0, 0, 0, 1])
+    np.testing.assert_equal(expected, labels)
+
+  def test_pre(self):
+    embeddings = [
+      [1, 2],
+      [3, -1],
+      [1, 1],
+      [-2, -1],
+      [0, 1],
+      [-2, 0],
+      ]
+    for embedding in embeddings:
+      labels = self.multi_stage.streaming_predict(np.array(embedding))
+    labels = utils.enforce_ordered_labels(labels)
+    expected = np.array([0, 1, 0, 2, 0, 1])
+    np.testing.assert_equal(expected, labels)
+
+
+if __name__ == "__main__":
+  unittest.main()
